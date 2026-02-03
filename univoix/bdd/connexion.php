9f197c52d348a4064785a5a6956b5a6cae76dbdf
lucaregi95 <lucaregi95@gmail.com>
2026-02-03 15:28:04 +0100

    Delete univoix directory

--- univoix/bdd/connexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-try {
-    $connexion = new PDO('mysql:host=localhost;dbname=//a changer;charset=utf8', '//a changer;charset=utf8','//a changer');
-} catch (PDOException $e) {
-    echo ("Erreur :".$e->getMessage());
-}
